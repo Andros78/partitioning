0cf92eac1cdc8695a0bd45e6be8c6fb6bd85373d
Andros78 <andresbodenheimer@gmail.com>
2021-05-18 00:39:56 +0200

    Commit SBM

--- SBM.py
+++ SBM.py
@@ -1,62 +1,67 @@
 import numpy as np
 
 class SBM:
-    """where p,q probabilities of SBM, n total number of vertices
-         and k total number of communities
+    """ 
+    Génère un graphe aléatoire type Stochastic Block Model. 
+
+    Les paramètres en entrée sont les suivants:
+    p: la probabilité des liens inter-communauté,
+    q: la probabilité d'un lien intra-communauté
+    n: le nombre de noeud
+    k: le nombre de communautés
     """
     def __init__(self, p, q, n, k):
-        self.N_communities=k
+        self.k=k
         self.p=p
         self.q=q
-        self.N_vertices= n + k - n%k if n%k!=0 else n #ainsi n est divisible par k
-        #print("n:", self.N_vertices)
-        self.len_communities=int(self.N_vertices / self.N_communities)
-        self.A=None  #probability matrix
-        self.M=np.zeros((self.N_vertices, self.N_vertices), dtype=np.int) #adjancency matrix  
+        self.n= n + k - n%k if n%k!=0 else n    # ainsi n est divisible par k
+        self.len_communities=int(self.n / self.k)
+
         self.Probability_Matrix()
         self.Adjacency_Matrix()
-        self.len=len(self.M)
-        self.Degree_mat=None
-        self.L=None
-        self.Ln=None
-        self.Laplacien_Matrix()
-        
-            
-    def Degree_Matrix(self):
-        self.Degree_mat=np.eye(self.len)
-        for i in range(self.len):
-            self.Degree_mat[i][i]=sum(self.M[i])
-
-    def Laplacien_Matrix(self):
         self.Degree_Matrix()
-        self.L=self.Degree_mat - self.M
+        self.Laplacian_Matrix()
         self.Normalized_Laplacian()
-
-    def Normalized_Laplacian(self):
-        Do=np.eye(self.len)
-        for i in range(self.len):
-            Do[i][i]=self.Degree_mat[i][i]**-1/2
-        self.Ln = np.dot(np.dot(Do, self.L), Do)
+    
 
     def Probability_Matrix(self):
+        self.P=None  #Matrice de probabilité
         J=np.ones((self.len_communities, self.len_communities))
-        tab=[None]*self.N_communities
-        for i in range(self.N_communities):
-            tab[i]=np.block([(self.p*J if i==j else self.q*J) for j in range(self.N_communities)])
-        self.A=np.block([[tab[i]] for i in range(self.N_communities)]) - self.p*np.eye(self.N_vertices)
-        
+        tab=[None]*self.k
+        for i in range(self.k):
+            tab[i]=np.block([(self.p*J if i==j else self.q*J) for j in range(self.k)])
+        self.P=np.block([[tab[i]] for i in range(self.k)]) - self.p*np.eye(self.n)
+    
     def Adjacency_Matrix(self):
-        L=np.random.random(size=(self.N_vertices, self.N_vertices))
-        for i in range(self.N_vertices):
+        self.M=np.zeros((self.n, self.n), dtype=np.int) #Matrice d'Adjacence
+        L=np.random.random(size=(self.n, self.n))
+        for i in range(self.n):
             for j in range(i+1):
-                self.M[i][j]= 1 if L[i][j] <= self.A[i][j] else 0
+                self.M[i][j]= 1 if L[i][j] <= self.P[i][j] else 0
                 self.M[j][i]=self.M[i][j]
 
+    def Degree_Matrix(self):
+        self.D=np.eye(self.n) #Matrice des degrés
+        for i in range(self.n):
+            self.D[i][i]=sum(self.M[i])
+
+    def Laplacian_Matrix(self):
+        self.L=self.D - self.M
+
+    def Normalized_Laplacian(self):
+        Do=np.eye(self.n)
+        for i in range(self.n):
+            Do[i][i]=self.D[i][i]**-1/2
+        self.Ln = np.dot(np.dot(Do, self.L), Do)
+
+    
+
 """
+#Tester le SBM: 
 model=SBM(0.9, 0.1, 7, 3)
 model.Probability_Matrix()
 model.Adjacency_Matrix()
-print(model.N_vertices)
+print(model.n)
 print(model.A)
 print(model.M)
 """
