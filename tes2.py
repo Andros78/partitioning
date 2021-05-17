41a0c464fdcfce9351ca48bcfc873f1783cf97cc
Andros78 <andresbodenheimer@gmail.com>
2021-05-18 01:13:17 +0200

    delete garbage

--- tes2.py
+++
@@ -1,3 +0,0 @@
-import numpy as np
-a=np.array([[2,2,2], [1,1,1], [1,1,1]])
-print(np.dot(np.dot(a,a), a))
--- test.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-class test_SBM:
-    def __init__(self, output_algo):
-        self.output_algo=output_algo
